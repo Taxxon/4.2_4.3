06991bc096587128b186c0a4853cddf120d0e66c
Taxxon <te15.emil.kack@umea.nti.se>
2017-11-15 14:25:21 +0100

    Klar med student och person

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,34 @@
 /**
- * Created by emka15 on 2017-11-15.
+ * Created by Emil Käck on 2017-11-15.
  */
 public class Person {
+
+    private String name;
+    private String address;
+
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
